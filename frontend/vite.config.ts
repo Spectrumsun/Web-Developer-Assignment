ddb40bc36c20e6cafde333c224de599233d96dbd
Spectrum Sun <Spectrumsun@users.noreply.github.com>
2025-09-05 17:16:27 +0100

    add frontend and backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,5 +10,9 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/test/test.config.ts",
   },
-  base: './'
+  base: '/',
+  build: {
+    outDir: '../frontend/dist', // ensure assets are in publish folder
+    emptyOutDir: true
+  }
 });
