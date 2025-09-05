013ea9363576633d47ef7c888059d6ac02c9eea8
Spectrum Sun <Spectrumsun@users.noreply.github.com>
2025-09-05 11:07:51 +0100

    add frontend and backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
-import lineClamp from '@tailwindcss/line-clamp'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss({
-      config: {
-        plugins: [lineClamp],
-      },
-    }),],
+  plugins: [react(),  tailwindcss()],
 })
